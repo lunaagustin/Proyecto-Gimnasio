4b99f8dc4a1e25e2c7e6704469d1aaf9077ce78a
enzosassari <enzosassari@hotmail.com>
2025-11-22 17:29:00 -0300

    cambios en entrenador

--- src/entrenador/entrenador.module.ts
+++ src/entrenador/entrenador.module.ts
@@ -7,9 +7,10 @@ import { Alumno } from 'src/alumno/entities/alumno.entity';
 import { Rutina } from 'src/rutina/entities/rutina.entity';
 import { Ejercicio } from 'src/ejercicio/entities/ejercicio.entity';
 import { Entrenador } from './entities/entrenador.entity';
+import { UsuarioModule } from 'src/usuario/usuario.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Entrenador,Usuario,Alumno,Rutina,Ejercicio])], /*entidades que voy a estar utilizando*/
+  imports: [TypeOrmModule.forFeature([Entrenador,Usuario,Alumno,Rutina,Ejercicio]), UsuarioModule], /*entidades que voy a estar utilizando*/
   controllers: [EntrenadorController],
   providers: [EntrenadorService],
   exports: [EntrenadorService]
--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -4,6 +4,7 @@ import { Usuario } from './entities/usuario.entity';
 import { UsuarioService } from './usuario.service';
 import { UsuarioController } from './usuario.controller';
 
+
 @Module({
   imports: [TypeOrmModule.forFeature([Usuario])],
   controllers: [UsuarioController],
